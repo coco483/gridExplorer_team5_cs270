cf28ec12a31fd35c5212919e02ec253b4adc3af0
runammm <163241135+runammm@users.noreply.github.com>
2024-03-29 02:14:57 +0900

    Update gridExplore.py

--- gridExplore.py
+++ gridExplore.py
@@ -3,6 +3,9 @@ from roboControl import Robot
 ##example code
 if __name__ == '__main__':
     robo = Robot()
-    robo.go_forward()
-    if robo.is_facing_block():
-        pass
+    box1_x, box1_y = robo.scan_forward()
+    if box1_x == -1 and box1_y == -1 : 
+        robo.scan_side()
+        for i in range(3) :
+            robo.scan_side()
+            robo.move_straight()
