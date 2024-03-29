e2b4a1cca8d5ee7b31341080c6dafc0af82f3a57
runammm <163241135+runammm@users.noreply.github.com>
2024-03-30 01:34:25 +0900

    Update gridExplore.py

--- gridExplore.py
+++ gridExplore.py
@@ -1,11 +1,52 @@
 from roboControl import Robot
 
-##example code
-if __name__ == '__main__':
-    robo = Robot()
-    box1_x, box1_y = robo.scan_forward()
-    if box1_x == -1 and box1_y == -1 : 
-        robo.scan_side()
-        for i in range(3) :
-            robo.scan_side()
+robo = Robot()
+b1_x, b1_y = robo.scan_forward()
+b2_x, b2_y = (0, 0)
+
+if b1_x == -1 and b1_y == -1 : 
+    b1_x, b1_y = robo.scan_side()
+    for i in range(3) :
+        robo.go_forward()
+        if b1_x != -1 and b1_y != -1 :
+            b2_x, b2_y = robo.scan_side()
+        else :
+            b1_x, b1_y = robo.scan_side()
+
+    if b2_x == -1 and b2_y == -1 :
+        if b1_y == 3 :
+            for i in range(3) :
+                robo.go_back()
+            robo.turn_left()
+            for i in range(5) :
+                robo.go_back()
+                b2_x, b2_y = robo.scan_side()
+        
+        else :
+            robo.turn_right()
+            for i in range(5) :
+                robo.go_forward()
+                b2_x, b2_y = robo.scan_side()
+    
+    else :
+        pass
+
+else :
+    robo.scan_side()
+    if b2_x != -1 and b2_y != -1 :
+        if b1_y == 1 :
+            robo.turn_right()
             robo.go_forward()
+            robo.turn_left()
+            robo.go_forward()
+        else :
+            robo.go_forward()
+            robo.turn_right()
+            robo.go_forward()
+            robo.turn_left()
+        
+    else :
+        robo.turn_left()
+        for i in range(5) :
+            robo.go_back()
+            b2_x, b2_y = robo.scan_side()
