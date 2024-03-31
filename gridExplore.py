765fd324a3dcb63718449ebb856138c3549503cf
runammm <163241135+runammm@users.noreply.github.com>
2024-03-31 17:40:57 +0900

    Update gridExplore.py

--- gridExplore.py
+++ gridExplore.py
@@ -23,32 +23,35 @@ if b1_x == -1 and b1_y == -1 :
             for i in range(5) :
                 robo.go_back()
                 b2_x, b2_y = robo.scan_side()
+            for i in range(5) :
+                robo.go_forward()
+            robo.turn_right()
         
         else :
             robo.turn_right()
             for i in range(5) :
                 robo.go_forward()
                 b2_x, b2_y = robo.scan_side()
+            for i in range(5) :
+                robo.go_back()
+            robo.turn_left()
+            for i in range(3) :
+                robo.go_back()
     
     else :
-        pass
+        for i in range(3) :
+            robo.go_back()
 
 else :
     robo.scan_side()
     if b2_x != -1 and b2_y != -1 :
-        if b1_y == 1 :
-            robo.turn_right()
-            robo.go_forward()
-            robo.turn_left()
-            robo.go_forward()
-        else :
-            robo.go_forward()
-            robo.turn_right()
-            robo.go_forward()
-            robo.turn_left()
+        pass
         
     else :
         robo.turn_left()
         for i in range(5) :
             robo.go_back()
             b2_x, b2_y = robo.scan_side()
+        for i in range(5) :
+            robo.go_forward()
+        robo.turn_right()
