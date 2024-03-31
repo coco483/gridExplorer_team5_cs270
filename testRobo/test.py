10a50e87a4ee57a037f5ea25bf2818094ceeb19e
coco483 <151052125+coco483@users.noreply.github.com>
2024-03-31 19:15:21 +0900

    fix position bug

--- testRobo/test.py
+++ testRobo/test.py
@@ -65,9 +65,9 @@ class CustomRobot(Robot):
         self.direction='E'
     elif self.direction=='E':
         self.direction='S'
-    if self.direction=='S':
+    elif self.direction=='S':
         self.direction='W'
-    if self.direction=='W':
+    elif self.direction=='W':
         self.direction='N'
 
   def is_grid_red(self):
@@ -108,6 +108,7 @@ class CustomRobot(Robot):
     while(self.hubo.front_is_clear()):
       self.go_forward()
       count+=1
+    #print("position: ", end="")
     #print(self.position)
     if self.direction=='N':
       boxpos = (self.position[0], self.position[1]+1)
@@ -117,13 +118,14 @@ class CustomRobot(Robot):
       boxpos = (self.position[0], self.position[1]-1)
     elif self.direction=='E':
       boxpos = (self.position[0]+1, self.position[1])
-    if (boxpos[0] < 5) and (boxpos[0]>=0) and (boxpos[1]>=0) and (boxpos[1] <3):
+    if (boxpos[0] <= 5) and (boxpos[0]>=0) and (boxpos[1]>=0) and (boxpos[1] <=3):
       for i in range(count):
         self.go_back()
       self.turn_left()
       self.boxes.append(boxpos)
+      print("boxpos: ", end="")
       print(boxpos)
-      return self.position
+      return boxpos
     for i in range(count):
       self.go_back()
     self.turn_left()
@@ -134,7 +136,31 @@ class CustomRobot(Robot):
     if self.position[0]<0 or self.position[0]>5 or self.position[1]<0 or self.position[1]>3:
       self.position=(-1,-1)
 
+  def goto(self,x,y):
+    dx= x-self.position[0]
+    dy= y-self.position[1]
 
+    if dx>=0:
+      while self.direction!='E':
+          self.turn_left()
+      for i in range(dx):
+          self.go_forward()
+    else:
+      while self.direction!='W':
+          self.turn_left()
+      for i in range(-dx):
+          self.go_forward()
+    
+    if dy>=0:
+      while self.direction!="N":
+          self.turn_left()
+      for i in range(dy):
+          self.go_forward()
+    else:
+      while self.direction!="S":
+          self.turn_left()
+      for i in range(-dy):
+          self.go_forward()
 
 
 robo = CustomRobot(0,0)
@@ -148,74 +174,15 @@ robo = CustomRobot(0,0)
 # hubo.front_is_clear() - box (hubo cannot distinguish end of map and box walls)
 
 ### implement here
-#robo = Robot()
-b1_x, b1_y = robo.scan_forward()
-b2_x, b2_y = (0, 0)
-r1_x, r1_y = (0, 0)
-r2_x, r2_y = (0, 0)
-#time.sleep(5)
-if b1_x == -1 and b1_y == -1 : 
-    b1_x, b1_y = robo.scan_side()
-    for i in range(3) :
-        robo.go_forward()
-        if b1_x != -1 and b1_y != -1 :
-            b2_x, b2_y = robo.scan_side()
-        else :
-            b1_x, b1_y = robo.scan_side()
-
-    if b2_x == -1 and b2_y == -1 :
-        print("if line 1 started")
-        time.sleep(2)
-        if b1_y == 3 :
-            print("if line 1-1")
-            for i in range(3) :
-                time.sleep(2)
-                robo.go_back()
-            robo.turn_left()
-            for i in range(5) :
-                time.sleep(2)
-                robo.go_back()
-                b2_x, b2_y = robo.scan_side()
-            for i in range(5) :
-                time.sleep(2)
-                robo.go_forward()
-            robo.turn_right()
-        
-        else :
-            print("if line 1: else")
-            robo.turn_right()
-            for i in range(5) :
-                robo.go_forward()
-                b2_x, b2_y = robo.scan_side()
-            for i in range(5) :
-                robo.go_back()
-            robo.turn_left()
-            for i in range(3) :
-                robo.go_back()
-    
-    else :
-        for i in range(3) :
-            robo.go_back()
-
-else :
-    robo.scan_side()
-    if b2_x != -1 and b2_y != -1 :
-        pass
-        
-    else :
-        robo.turn_left()
-        for i in range(5) :
-            robo.go_back()
-            b2_x, b2_y = robo.scan_side()
-        for i in range(5) :
-            robo.go_forward()
-        robo.turn_right()
-
-print(b1_x, b1_y, "fdsfds", b2_x, b2_y)
 
 
-print(robo.red)
-print(robo.boxes)
+
+
+print("b1 is ", b1_x, b1_y, "and b2 is", b2_x, b2_y)
+
+
+#print(robo.red)
+#print(robo.boxes)
 
 ### correct outputs
 # 1: (1, 1, B) (4, 2, B) (3, 0, R) (2, 3, R)
